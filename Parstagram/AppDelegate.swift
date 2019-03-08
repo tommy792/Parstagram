2bc06ebf9923eddd4d901b10b6d65da475bda43b
Tomas Nunez <TomasNunez@tomass-MacBook-Air.local>
2019-03-07 17:27:35 -0800

    s

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Initialize Parse
+        // Set applicationId and server based on the values in the Heroku settings.
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "myAppId"
+                configuration.server = "https://protected-shore-48107.herokuapp.com/parse"
+            })
+        )
+        
+        
+        
         return true
     }
 
---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,34 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by tomas nunez on 3/7/19.
+//  Copyright © 2019 tomas nunez. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
